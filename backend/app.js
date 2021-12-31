f6e46e7c2dbd2d043c991dafdf34a3c2cc0056ce
Haruna Oseni <harunaoseni23@gmail.com>
2021-12-31 04:45:07 -0600

    routes and model for features created to be impl

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,28 @@
+// modules required for server
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-const api = process.env.API_URL;
-const productRoutes = require("./routes/products");
+// Routes
+const productsRoutes = require("./routes/products");
+const ordersRoutes = require("./routes/orders");
+const usersRoutes = require("./routes/users");
+const categoriesRoutes = require("./routes/categories");
 
+// api for routes
+const api = process.env.API_URL;
+
+// middleware
 app.use(express.json()); // means we can receive json data from the client
 app.use(morgan("dev")); // log every request to the console
-app.use(`${api}/product`, productRoutes);
+app.use(`${api}/product`, productsRoutes);
+app.use(`${api}/order`, ordersRoutes);
+app.use(`${api}/user`, usersRoutes);
+app.use(`${api}/category`, categoriesRoutes);
 
+// connect to mongodb
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -20,6 +32,7 @@ mongoose
     console.log(`Error connecting to database: ${error}`);
   });
 
+// creaing a server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
