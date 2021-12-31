9f8ccfd062a40edc100199ffb161388fdd21d7b9
Haruna Oseni <harunaoseni23@gmail.com>
2021-12-31 04:23:59 -0600

    get post route model implm product

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv/config");
-const Product = require("./models/product");
 
 const api = process.env.API_URL;
+const productRoutes = require("./routes/products");
 
 app.use(express.json()); // means we can receive json data from the client
 app.use(morgan("dev")); // log every request to the console
+app.use(`${api}/product`, productRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -17,36 +19,6 @@ mongoose
   .catch((error) => {
     console.log(`Error connecting to database: ${error}`);
   });
-
-app.get(`${api}/product`, async (req, res) => {
-  const productList = await Product.find();
-  if (productList.length > 0) {
-    res.status(200).json(productList);
-  } else {
-    res.status(404).json({ message: "No products found" });
-  }
-});
-
-app.post(`${api}/product`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
-
-  product
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Error saving product",
-        error: error,
-        success: false,
-      });
-    });
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ backend/routes/products.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product");
+
+router.get(`/`, async (req, res) => {
+  const productList = await Product.find();
+  if (productList.length > 0) {
+    res.status(200).json(productList);
+  } else {
+    res.status(404).json({ message: "No products found" });
+  }
+});
+
+router.post(`/`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error saving product",
+        error: error,
+        success: false,
+      });
+    });
+});
+
+//export router module
+module.exports = router;
