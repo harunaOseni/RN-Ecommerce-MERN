901568acac509d81364be0a2f0d983274520b980
Haruna Oseni <harunaoseni23@gmail.com>
2022-01-01 23:27:26 -0600

    get and delete categories route implemented

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -14,4 +14,42 @@ router.get("/", async (req, res) => {
   res.send(categoryList);
 });
 
+router.post("/", async (req, res) => {
+  const newCategory = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  newCategory
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error saving category",
+        error: err,
+        success: false,
+      });
+    });
+});
+
+router.delete("/:id", async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res.status(404).json({
+      message: "Category not found",
+      success: false,
+    });
+  } else {
+    await Category.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Category deleted",
+      success: true,
+    });
+  }
+});
+
 module.exports = router;
