0154b9b9594eb91595732e47da3fef8f0b59f340
Haruna Oseni <harunaoseni23@gmail.com>
2022-01-04 07:00:06 -0600

    get user route impl with password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get("/", async (req, res) => {
   const userList = await User.find();
@@ -12,6 +13,48 @@ router.get("/", async (req, res) => {
   }
 
   res.send(userList);
+});
+
+router.get("/:id", (req, res) => {
+  // get user data excluding password
+  User.findById(req.params.id, "-passwordHash")
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  newUser
+    .save()
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        error: err,
+        message: "Error while saving user",
+      });
+    });
 });
 
 module.exports = router;
