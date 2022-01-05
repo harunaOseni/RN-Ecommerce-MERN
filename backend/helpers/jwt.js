961b8f27397269babff5a52b07046830c6216005
Haruna Oseni <harunaoseni23@gmail.com>
2022-01-05 01:04:01 -0600

    excluded some rest api from requiring authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,7 +2,18 @@ const expressJwt = require("express-jwt");
 require("dotenv/config");
 
 function authJwt() {
-  return expressJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] });
+  const secret = process.env.JWT_SECRET;
+  const api = process.env.API_URL;
+  return expressJwt({ secret, algorithms: ["HS256"] }).unless({
+    path: [
+      // public routes that don't require authentication
+      //regex for products
+      `${api}/user/login`,
+      `${api}/user/register`,
+      { url: /\/api\/v1\/product(.*)/, methods: ["GET"] },
+      { url: /\/api\/v1\/category(.*)/, methods: ["GET"] },
+    ],
+  });
 }
 
 module.exports = authJwt;
