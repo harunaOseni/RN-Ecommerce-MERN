9a7d8493eb4531b3c24e5187f49f4ace89418a2d
Haruna Oseni <harunaoseni23@gmail.com>
2022-01-17 01:04:59 -0600

    product container margin corrected

--- rn-ecommerce-mern/Screens/Products/ProductCard.js
+++ rn-ecommerce-mern/Screens/Products/ProductCard.js
@@ -10,7 +10,7 @@ import {
 
 const { width } = Dimensions.get("window");
 
-const ProductCard = ({ item }) => {
+const ProductCard = ({ item }) => { 
   return (
     <View style={styles.container}>
       <Image
