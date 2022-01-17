eb6e2a64f5491415a0c8386f17049cf2c530ed3d
Haruna Oseni <harunaoseni23@gmail.com>
2022-01-17 00:07:38 -0600

    header component created

---
+++ rn-ecommerce-mern/Shared/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Image, SafeAreaView, View } from "react-native";
+
+const Header = () => {
+    return (
+        <SafeAreaView style={styles.header}>
+        </SafeAreaView>
+    )
+}
